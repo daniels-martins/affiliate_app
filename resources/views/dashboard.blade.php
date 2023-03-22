ce4d085bf9382c37435e89adc26a421730eee145
Martins Daniels <danielsmrtns@gmail.com>
2023-03-22 09:11:50 -0400

    HG: Don't show  upline data for downlines

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,10 +13,10 @@
                             Brief Summary
                         </div>
                         {{-- enable link to admin panel --}}
-                        <div class="admin-panel-link">
+                        {{-- <div class="admin-panel-link">
                             <a class="text-white bg-blue-500 py-2 px-4 hover:bg-blue-400  rounded"
                                 href="{{ route('admin_panel') }}"> View Admin Panel</a>
-                        </div>
+                        </div> --}}
                     </div>
                     <br><br>
                     <div class="referral upline bg-gray-200 p-3 rounded shadow-sm text-gray-700 my-5">
@@ -26,17 +26,7 @@
                             {{ route('register', ['ref' => auth()->user()->ref_code ?? 000]) }}</a> <br>
 
                     </div>
-                    <div class="upline bg-blue-400 p-3 rounded shadow-sm text-gray-700 my-5">
-                        <p class="text-xl">My Upline Data </p> <br>
-                        Super code: {{ auth()->user()->super_code ?? 'No super_code' }}<br><br>
-                        Upline:
-                        @empty(auth()->user()->getUpline('name'))
-                            {{ 'No upline' }}
-                        @else
-                            {{ ucfirst(auth()->user()->getUpline('name')) }}
-                        @endempty
-                        <br><br>
-                    </div>
+                   
                     @if (auth()->user()->getDownlines())
                         <div class="downlines bg-green-400 p-3 rounded shadow-sm text-gray-700 my-5">
                             <p class="text-xl">My Downlines ({{ auth()->user()->getDownlines()->count() }})</p> <br>
